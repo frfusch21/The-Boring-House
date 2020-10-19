509f0387d5ff1422c4faa919648096020aafa52f
Fay <56527190+frfusch21@users.noreply.github.com>
2020-10-19 13:34:40 +0700

    Add files via upload

---
+++ WindowsFormsApp2/WindowsFormsApp2/POINT3D.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp2
+{
+    public class POINT3D
+    {
+        public double x;
+        public double y;
+        public double z;
+        public double w = 1;
+    }
+}
